ae1936a7054e1f14f1d26014974a062ef7d3975d
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-20 12:29:16 -0300

    7-Insere table em um scrollPane. Configura o layout de ListaEventosPanel
    Tive que inserir via código a table dentro do scrollPane.

--- src/agenda/gui/ListaEventosPanel.java
+++ src/agenda/gui/ListaEventosPanel.java
@@ -1,17 +1,31 @@
 package agenda.gui;
 
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTable;
+import java.awt.BorderLayout;
+import javax.swing.JButton;
 
 /**
  * Lista os eventos cadastrados no sistema.
  * 
  */
 public class ListaEventosPanel extends JPanel {
+	private JTable table;
 
 	/**
 	 * Create the panel.
 	 */
 	public ListaEventosPanel() {
+		setLayout(new BorderLayout(0, 0));
+		
+		JScrollPane scrollPane = new JScrollPane();
+		add(scrollPane, BorderLayout.CENTER);
+		
+		table = new JTable();
+		scrollPane.add(table);
+		
+		
 
 	}
 
