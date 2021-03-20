ca83e72ae489052e21fb5c31f6c82a66ab9341d5
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-20 16:35:00 -0300

    19- Criação do método getEventos() que recupera os dados de txt.
    Recupera de um txt ("eventos.txt") os eventos registrados.

--- src/agenda/io/AgendaIO.java
+++ src/agenda/io/AgendaIO.java
@@ -1,10 +1,14 @@
 package agenda.io;
 
+import java.io.BufferedReader;
 import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.io.FileWriter;
+import java.util.Vector;
 
+import agenda.utils.PeriodicidadeEnum;
 import agenda.vo.Evento;
 
 /**
@@ -42,6 +46,54 @@ public class AgendaIO {
 			
 			throw new Exception("Problemas na gravação do arquivo");
 		}
+	}
+	
+	
+	/**
+	 * Recupera de um txt ("eventos.txt") os eventos registrados.
+	 * @return {@link Vector}< {@link Vector}< Object > >
+	 * @throws Exception
+	 */
+	public Vector<Vector<Object>> getEventos() throws Exception{
+		
+		Vector<Vector<Object>> lista = new Vector<Vector<Object>>();
+		
+		try {
+			
+			File file = new File(DIRETORIO, ARQUIVO);
+			FileReader fileReader = new FileReader(file);
+			BufferedReader bufferedReader = new BufferedReader(fileReader);
+			
+			String linha = bufferedReader.readLine();
+			Vector<Object> eventoVector = null;
+			while( linha != null) {
+				
+				eventoVector = new Vector<Object>();
+				String[] tokens = linha.split(";");
+				
+				eventoVector.add(tokens[0]); // Data
+				eventoVector.add(tokens[1]); // Descrição do evento
+				eventoVector.add(PeriodicidadeEnum.valueOf(tokens[2])); // Periodicidade
+				eventoVector.add(tokens[3]); // Email para encaminhar
+				eventoVector.add(tokens[4].equals("1") ? "LIGADO" : "DESLIGADO"); // alarme
+				
+				lista.add(eventoVector);
+				linha = bufferedReader.readLine();
+			}
+			
+			bufferedReader.close();
+			
+		} catch(FileNotFoundException fnfe) {
+			
+			throw new Exception("Arquivo não encontrado.");
+			
+		} catch(Exception ex) {
+			
+			throw new Exception("Problemas de leitura no arquivo de eventos.");
+		}
+		
+		
+		return lista;
 	}
 
 }
