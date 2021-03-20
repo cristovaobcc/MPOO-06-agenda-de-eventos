e95c0b41b0f15e7d723ae6a385b909b4de122317
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-20 11:22:00 -0300

    02-Criação de painel que recebe os dados de novo evento de calendário.

---
+++ src/agenda/gui/CadastroEventoPanel.java
@@ -0,0 +1,15 @@
+package agenda.gui;
+
+import javax.swing.JPanel;
+
+public class CadastroEventoPanel extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public CadastroEventoPanel() {
+		setLayout(null);
+
+	}
+
+}
