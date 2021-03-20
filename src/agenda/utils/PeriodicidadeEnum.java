ae6675d189dc879f2cb5fe6b5e68a1fd14addbd1
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-20 13:17:36 -0300

    14-Insere PeriodicidadeEnum para representar periodicidade de Evento.

---
+++ src/agenda/utils/PeriodicidadeEnum.java
@@ -0,0 +1,12 @@
+/**
+ * 
+ */
+package agenda.utils;
+
+/**
+ * Enum que representa as opções de frequencia disponíveis para os eventos.
+ *
+ */
+public enum PeriodicidadeEnum {
+	UNICO, SEMANAL, MENSAL;
+}
