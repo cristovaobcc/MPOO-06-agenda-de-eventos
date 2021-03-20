dfeeca693be89a02c6f59fa5681a56f7f730e33e
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-20 13:10:45 -0300

    12 - Cria a classe Evento dentro de pacote VO
    Sobre VO veja: https://www.martinfowler.com/bliki/ValueObject.html
    
    Veja tb: https://stackoverflow.com/questions/1612334/difference-between-dto-vo-pojo-javabeans

---
+++ src/agenda/vo/Evento.java
@@ -0,0 +1,11 @@
+package agenda.vo;
+
+/**
+ * Representa um evento de agenda e contém seus dados de data, descrição, periodicidade, email para
+ * encaminhar e alarme.
+ * 
+ *
+ */
+public class Evento {
+
+}
