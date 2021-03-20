0f3271ce18dc8490ff9ba3448e22a036770c853b
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-20 13:15:14 -0300

    13 - Insere os atributos da classe Evento.

--- src/agenda/vo/Evento.java
+++ src/agenda/vo/Evento.java
@@ -1,11 +1,25 @@
 package agenda.vo;
 
+import java.io.Serializable;
+import java.util.Date;
+
 /**
  * Representa um evento de agenda e contém seus dados de data, descrição, periodicidade, email para
  * encaminhar e alarme.
  * 
  *
  */
-public class Evento {
+public class Evento implements Serializable{
+
+	private static final long serialVersionUID = 1L;
+	
+	private Date dataEvento;
+	private String descEvento;
+	private PeriodicidadeEnum periodicidade;
+	private String emailEncaminhar;
+	private int alarme;
+	
+		
+	
 
 }
