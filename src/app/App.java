fe43fc9798e65a17ee5ac27c379bfd81569aae79
cristovaobcc <cristovao.bcc@gmail.com>
2021-03-20 23:24:44 -0300

    26 -Cria classe principal App e arquivo eventos.txt
    Até aqui, a parte de cadastro funciona, mas a criação automática de
    arquivo e a listagem de eventos registradas no arquivo eventos.txt, não
    está funcionando ainda.

---
+++ src/app/App.java
@@ -0,0 +1,23 @@
+/**
+ * 
+ */
+package app;
+
+import agenda.gui.AgendaFrame;
+
+/**
+ * Classe principal do sistema Agenda.
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		AgendaFrame agendaFrame = new AgendaFrame();
+		agendaFrame.setVisible(true);
+
+	}
+
+}
